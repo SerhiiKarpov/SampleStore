4f0e2c80e39b43f224f3a8dd105144a84eec4942
Serhii Karpov <sergii.karpov@altexsoft.com>
2026-05-30 10:40:05 +0300

    Added ability to create product with photo.

--- SampleStore.UI/Pages/Products/Create.cshtml.cs
+++ SampleStore.UI/Pages/Products/Create.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using SampleStore.Common.Extensions;
@@ -22,6 +25,10 @@ public class CreateModel : PageModelBase
     [BindProperty]
     public Product? Product { get; set; }
 
+    [BindProperty]
+    [Display(Name = "Photo")]
+    public IFormFile? PhotoFile { get; set; }
+
     public override string Title => "Create";
 
     public IActionResult OnGet()
@@ -34,6 +41,21 @@ public class CreateModel : PageModelBase
         if (!ModelState.IsValid)
         {
             return Page();
+        }
+
+        if (PhotoFile is { Length: > 0 })
+        {
+            using var memoryStream = new MemoryStream();
+            await PhotoFile.CopyToAsync(memoryStream);
+
+            var photo = new Photo
+            {
+                Image = memoryStream.ToArray(),
+                MimeType = PhotoFile.ContentType,
+            };
+
+            _unitOfWork.GetRepository<Photo>().Add(photo);
+            Product!.PhotoId = photo.Id;
         }
 
         _unitOfWork.GetRepository<Product>().Add(Product!);
