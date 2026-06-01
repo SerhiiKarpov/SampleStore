21cef16226f41ba37409d1a285970739b178946a
Serhii Karpov <sergii.karpov@altexsoft.com>
2026-06-01 11:43:42 +0300

    Update home page to display top 10 products

--- SampleStore.UI/Pages/Index.cshtml.cs
+++ SampleStore.UI/Pages/Index.cshtml.cs
@@ -1,10 +1,36 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+
+using SampleStore.Data;
+using SampleStore.Data.Entities.Domain;
 
 namespace SampleStore.UI.Pages;
 
 [AllowAnonymous]
 public class IndexModel : PageModelBase
 {
+    private readonly IQueryMaterializer _queryMaterializer;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public IndexModel(IUnitOfWork unitOfWork, IQueryMaterializer queryMaterializer)
+    {
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        _queryMaterializer = queryMaterializer ?? throw new ArgumentNullException(nameof(queryMaterializer));
+    }
+
+    public IList<Product> Products { get; set; } = [];
+
     public override string Title => "Home page";
+
+    public async Task OnGetAsync()
+    {
+        var productQuery = _unitOfWork.GetRepository<Product>().Query
+            .OrderBy(p => p.Name)
+            .Take(10);
+        Products = await _queryMaterializer.ToList(productQuery);
+    }
 }
